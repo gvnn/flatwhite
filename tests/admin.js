679591e2f62071782cb68f569e6b486f61debf93
gvnn <gvnnrulez@gmail.com>
2011-12-18 20:23:00 +1100

    removed some duplicate code in tests

--- tests/admin.js
+++ tests/admin.js
@@ -1,119 +1,61 @@
 var request = require("./request");
-var config = require("../src/config");
 var querystring = require("querystring");
-var http = require("http");
-var unit_test, client;
-var admin_num = 0;
+var unitTest, client;
+var adminNumber = 0;
 
-var start_test = function(test) {
-    unit_test = test;
-
+var startTest = function(test) {
+    unitTest = test;
     //get list
-    get_list(function(num) {
-        admin_num = num;
-
+    getList(function(num) {
+        adminNumber = num;
+        
         //first test: add admin
-        var post_data = querystring.stringify({
+        var postData = querystring.stringify({
             "email" : "admin@domain.com",
             "password": "password",
             "active": true
         });
-    
-        var post_options = {
-            host: '127.0.0.1',
-            port: config.server.port,
-            path: '/1/admin',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': post_data.length
-            }
-        };
-    
-        var post_req = http.request(post_options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                unit_test.equal(res.statusCode, 200);
-                response = JSON.parse(chunk);
-                unit_test.notEqual(response._id, null);
-                //move to get user
-                get_admin(unit_test, response);
-            });
+        
+        request.post('POST', 'admin', postData, function(res, chunk) {
+            unitTest.equal(res.statusCode, 200);
+            response = JSON.parse(chunk);
+            unitTest.notEqual(response._id, null);
+            //move to get user
+            getAdmin(unitTest, response);
         });
-    
-        post_req.write(post_data);
-        post_req.end();
     
     });
 };
 
-var update_admin = function(test, obj, callback) {
-    unit_test = test;
-    res_object = obj;
-    
-    var post_data = querystring.stringify({
+var updateAdmin = function(test, obj, callback) {
+    unitTest = test;
+    resObject = obj;
+    var postData = querystring.stringify({
         "email" : "admin@domain.com1",
         "password": "password1",
         "active": false
     });
-    
-    var post_options = {
-        host: '127.0.0.1',
-        port: config.server.port,
-        path: '/1/admin/' + res_object._id,
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': post_data.length
-        }
-    };
-
-    var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            unit_test.equal(res.statusCode, 200);
-            callback();
-        });
+    request.post('PUT', 'admin/' + resObject._id, postData, function(res, chunk) {
+        unitTest.equal(res.statusCode, 200);
+        callback();
     });
-
-    post_req.write(post_data);
-    post_req.end();
-    
 }
 
-var get_admin = function(test, obj) {
-    unit_test = test;
-    res_object = obj;
-    
-    update_admin(unit_test, obj, function() {
-    
-        var get_options = {
-            host: '127.0.0.1',
-            port: config.server.port,
-            path: '/1/admin/' + res_object._id,
-            method: 'GET'
-        };
-    
-        var get_req = http.request(get_options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                unit_test.equal(res.statusCode, 200);
-                
-                var response = JSON.parse(chunk);
-                
-                unit_test.notEqual(res_object.email, response.email);
-                unit_test.equal(response.password, null); //don't return password
-                delete_admin(unit_test, response); //move to delete user
-                
-            });
+var getAdmin = function(test, obj) {
+    unitTest = test;
+    resObject = obj;
+    updateAdmin(unitTest, obj, function() {
+        request.get('admin/' + resObject._id, function(res, chunk) {
+            unitTest.equal(res.statusCode, 200);
+            var response = JSON.parse(chunk);
+            unitTest.notEqual(resObject.email, response.email);
+            unitTest.equal(response.password, null); //don't return password
+            deleteAdmin(unitTest, response); //move to delete user
         });
-    
-        get_req.end();
-        
     });
 };
 
-var get_list = function(c) {
+var getList = function(c) {
     callback = c;
     request.get('admin/', function(res, chunk) {
         response = JSON.parse(chunk);
@@ -121,35 +63,20 @@ var get_list = function(c) {
     });
 };
 
-var delete_admin = function(test, obj) {
-    
-    unit_test = test;
-    res_object = obj;
-    
-    var delete_options = {
-        host: '127.0.0.1',
-        port: config.server.port,
-        path: '/1/admin/' + res_object._id,
-        method: 'DELETE'
-    };
-    
-    var delete_req = http.request(delete_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            unit_test.equal(res.statusCode, 200);
-            
-            get_list(function(num) {
-                unit_test.equal(admin_num, num);
-                unit_test.done();
-            });
-            
+var deleteAdmin = function(test, obj) {
+    unitTest = test;
+    resObject = obj;
+    request.post('DELETE', 'admin/' + resObject._id, '', function(res, chunk) {
+        unitTest.equal(res.statusCode, 200);
+        getList(function(num) {
+            unitTest.equal(adminNumber, num);
+            unitTest.done();
         });
     });
-    
-    delete_req.end();
 };
 
 //tryes to creat a new admin
-exports.test_admin = function(test) {
-    start_test(test);
+exports.testAdmin = function(test) {
+    test.expect(8);
+    startTest(test);
 }
--- tests/request.js
+++ tests/request.js
@@ -1,5 +1,4 @@
 var config = require("../src/config");
-var querystring = require("querystring");
 var http = require("http");
 
 var request = (function () {
@@ -10,51 +9,49 @@ var request = (function () {
     
     module.get = function(path, callback) {
         
-        var get_options = {
+        var getOptions = {
             host: config.server.ip,
             port: config.server.port,
             path: '/' + VERSION + '/' + path,
             method: 'GET'
         };
     
-        var get_req = http.request(get_options, function(res) {
+        var getRequest = http.request(getOptions, function(res) {
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
                 callback(res, chunk);
             });
         });
     
-        get_req.end();
-        
+        getRequest.end();
     };
     
-    module.post = function(method, path, post_data, callback) {
+    module.post = function(method, path, postData, callback) {
         
         if(method == '') {
             method = 'POST';
         }
         
-        var post_options = {
+        var postOptions = {
             host: config.server.ip,
             port: config.server.port,
             path: '/' + VERSION + '/' + path,
             method: method,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': post_data.length
+                'Content-Length': postData.length
             }
         };
         
-        var post_req = http.request(post_options, function(res) {
+        var postRequest = http.request(postOptions, function(res) {
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
                 callback(res, chunk);
             });
         });
-    
-        post_req.write(post_data);
-        post_req.end();
         
+        postRequest.write(postData);
+        postRequest.end();
     };
     
     return module;
