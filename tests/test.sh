5768d971ed6d6945bc0cb19f0103ff95099a7229
gvnn <gvnn@xgvnn.(none)>
2011-12-20 16:43:56 +1100

    removed useless echos

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
 clear
-
-echo "starting flatwhite"
 nohup node ../src/flatwhite.js > /dev/null & 
-
-echo "running modules test"
 nodeunit modules/auth.js
-
-echo "running api tests"
 nodeunit api/admin.js
-
-echo "stop node"
 kill -9 `ps -aef | grep 'node ../src/flatwhite.js' | grep -v grep | awk '{print $2}'`
