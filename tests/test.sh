88d7a5d4d49299e60dd74c6f613f65908db3f0f4
gvnn <gvnnrulez@gmail.com>
2012-01-30 23:12:57 +1100

    changed tests in .sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 clear
 nohup node ../src/flatwhite.js > /dev/null & 
-nodeunit modules/auth.js
 nodeunit api/admin.js
-nodeunit modules/items.js
 nodeunit api/items.js
-node api/upload.js
+nodeunit api/files.js
+nodeunit modules/auth.js
+nodeunit modules/items.js
 kill -9 `ps -aef | grep 'node ../src/flatwhite.js' | grep -v grep | awk '{print $2}'`
