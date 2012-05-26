9cc39ec41bb6f24fa6bac34d681bac9f056671a9
gvnn <gvnnrulez!>
2012-05-26 20:16:52 +1000

    test bash simplified

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 clear
-nohup node ../src/flatwhite.js > /dev/null & 
 nodeunit api/admin.js
 nodeunit api/items.js
 nodeunit api/files.js
 nodeunit modules/auth.js
 nodeunit modules/items.js
-kill -9 `ps -aef | grep 'node ../src/flatwhite.js' | grep -v grep | awk '{print $2}'`
